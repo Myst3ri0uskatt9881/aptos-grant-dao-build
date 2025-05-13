bd7a8378abd5dc8a9dc649dd615b06beeaab52fb
Katt Parkhurst <raynorkatt@gmail.com>
2025-05-13 15:07:12 -0400

    Add files via upload

---
+++ aptos-grant-dao-build/src/contracts/HelloWorld.move
@@ -0,0 +1,4 @@
+// Move smart contract example
+module AptosGrantDAO::HelloWorld {
+    public fun hello(): u8 { 42 }
+}
---
+++ aptos-grant-dao-build/src/frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <div>Hello from Aptos Grant DAO Frontend</div>;
+}
+
+export default App;
